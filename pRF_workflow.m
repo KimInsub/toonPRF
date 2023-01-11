a49b1d78df737cd6830d84c79bd9d0b9d9cc01ac
David Cardinal <github@cardinalphoto.com>
2023-01-10 21:15:14 -0800

    fix paths

--- pRF_workflow.m
+++ pRF_workflow.m
@@ -3,6 +3,7 @@
 % Modified by Insub 
 
 %% enviornment setting
+% Insub: This should either be explained or deleted: DJC
 % cd /oak/stanford/groups/kalanit/biac2/kgs/projects/insub/toonPRF
 % addpath(genpath('/Users/insubkim/spm12'));
 
@@ -10,39 +11,38 @@
 % initialize
 baseDir = initPath();
 
-% Vistasoft is the set of tools provided by Vistalab for analyzing MR
-% images
+% Vistasoft is the set of tools provided by Vistalab for analyzing MR images
 
-% clone Vistasoft from github
-if ~exist('./vistasoft', 'dir')
+% clone Vistasoft from github _if_ it isn't already in our path
+if isempty(which('vistaRootPath')) && ~exist(fullfile('.','vistasoft'), 'dir')
     system('git clone https://github.com/vistalab/vistasoft.git')
+    addpath(genpath(fullpath('.','vistasoft'))); % add vistasoft
 end
 
-addpath(genpath('./vistasoft')); % add vistasoft
 
 %% set path for data
 cd(baseDir)
 % set session information  
 % baseDir='/oak/stanford/groups/kalanit/biac2/kgs/projects/psych224/';
-expt    = '/data/subj02/';
+experimentData = fullfile('data','subj02');
 
 % paramPath =fullfile('Stimuli','8bars_params.mat');
 % imgPath = fullfile('Stimuli','8bars_images.mat');
 
-anatSubjPth = fullfile('./3DAnatomy','t1.nii.gz');
+anatSubjPth = fullfile('.','3DAnatomy','t1.nii.gz');
 setVAnatomyPath(anatSubjPth);
 
 %% initialize session
-pRF_init(baseDir, expt);
+pRF_init(baseDir, experimentData);
 
 %% View stimulus
-subjectDir=fullfile(baseDir,expt);
+subjectDir=fullfile(baseDir,experimentData);
 cd(subjectDir)
 
 % stim = load('./Stimuli/8bars_images.mat');
 % implay(stim.images,4)
 
-toon_showStim('./Stimuli/','8bars_images.mat')
+toon_showStim(fullfile('.','Stimuli','8bars_images.mat'))
 
 %% Quality check
 cd(subjectDir)
@@ -64,6 +64,6 @@ toon_motionCorrect(baseDir, expt);
 toon_2gray(baseDir, expt)
 
 %% run CSS pRF model
-imgPath= 'Stimuli/8bars_images.mat';
-paramPath = 'Stimuli/8bars_params.mat';
-toon_prfRun(baseDir, expt, [], paramPath, imgPath)
+imgPath= fullfile('Stimuli','8bars_images.mat');
+paramPath = fullfile('Stimuli','8bars_params.mat');
+toon_prfRun(baseDir, experimentData, [], paramPath, imgPath)
